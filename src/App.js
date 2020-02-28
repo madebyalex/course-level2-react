ab104a0876506454b610d88ec32f90d93a9d211c
Alexander <hello@alexander.works>
2020-02-28 23:40:25 +0200

    Using Context API: Passing values via UserContext.Provider

--- src/App.js
+++ src/App.js
@@ -1,41 +1,67 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import { UserContext } from './UserContext';
 import Header from './components/Header';
 import { ToggleRPC, ToggleRenderProps } from './utilities';
 import { Modal } from './elements';
+import User from './components/User';
 import './App.scss';
+
+class UserProvider extends Component {
+  state = {
+    id: '249',
+    name: 'Alex',
+    email: 'hello@alexander.works'
+  };
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <ToggleRenderProps
-          render={({ on, toggle }) => (
-            <div>
-              <Header extraClass={on} />
-              {/* {on && <h1>Show me!</h1>} */}
-              <button className='secondary' onClick={toggle}>
-                Switch header
-              </button>
-            </div>
-          )}
-        />
+    <UserProvider>
+      <div className='App'>
+        <header className='App-header'>
+          <ToggleRenderProps
+            render={({ on, toggle }) => (
+              <div>
+                <Header extraClass={on} />
+                {/* {on && <h1>Show me!</h1>} */}
+                <button className='secondary' onClick={toggle}>
+                  Switch header
+                </button>
 
-        <ToggleRPC>
-          {({ on, toggle }) => (
-            <Fragment>
-              {on ? (
-                <Modal on={on} toggle={toggle}>
-                  <h1>Hey! I'm from Modal</h1>
-                  <p>Welcome onboard!</p>
-                </Modal>
-              ) : (
-                <button onClick={toggle}>Show modal</button>
-              )}
-            </Fragment>
-          )}
-        </ToggleRPC>
-      </header>
-    </div>
+                <User />
+              </div>
+            )}
+          />
+
+          <ToggleRPC>
+            {({ on, toggle }) => (
+              <Fragment>
+                {on ? (
+                  <Modal on={on} toggle={toggle}>
+                    <h1>Hey! I'm from Modal</h1>
+                    <p>Welcome onboard!</p>
+                  </Modal>
+                ) : (
+                  <button onClick={toggle}>Show modal</button>
+                )}
+              </Fragment>
+            )}
+          </ToggleRPC>
+        </header>
+      </div>
+    </UserProvider>
   );
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { UserContext } from '../UserContext';
+
+export default class User extends Component {
+  render() {
+    return (
+      <UserContext.Consumer>
+        {context => (
+          <div>
+            <h1>{context.user.name}</h1>
+            <h3>
+              {context.user.email} &middot; {context.user.id}
+            </h3>
+          </div>
+        )}
+      </UserContext.Consumer>
+    );
+  }
+}
